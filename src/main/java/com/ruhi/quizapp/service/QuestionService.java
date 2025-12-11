2c4fc6f6534f7941f2fd8898df2d4ecad0a6d99c
Rahamatunnisa Shaik <rahamatunnisa1121@gmail.com>
2025-12-11 22:41:01 +0530

    Add HttpStatus in Response

--- src/main/java/com/ruhi/quizapp/service/QuestionService.java
+++ src/main/java/com/ruhi/quizapp/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.ruhi.quizapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ruhi.quizapp.dao.QuestionDao;
@@ -13,17 +16,36 @@ public class QuestionService {
 	
 	@Autowired
 	QuestionDao questionDao;
-	public List<Question> getAllQuestions() {
-		return questionDao.findAll();
+	
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		try {   
+			return new ResponseEntity<>(questionDao.findAll(),HttpStatus.OK);
+		}
+		catch(Exception e) {
+			e.printStackTrace(); //prints complete error details to console
+		}
+		return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
 	}
 	
-	public List<Question> getQuestionsByCategory(String category){
-		return questionDao.findByCategory(category);
+	public ResponseEntity<List<Question>> getQuestionsByCategory(String category){
+		try {
+			return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+		}
+		catch(Exception e) {
+			e.printStackTrace(); //prints complete error details to console
+		}
+		return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
 	}
 
-	public String addQuestion(Question question) {
+	public ResponseEntity<String> addQuestion(Question question) {
 		// TODO Auto-generated method stub
-		questionDao.save(question);
-		return "success!";
+		try {
+			questionDao.save(question);
+			return new ResponseEntity<>("success!",HttpStatus.CREATED);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Something went wrong on the server.",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
